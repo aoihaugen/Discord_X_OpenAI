759b0a708d73001d83e5b3d16e5203d8dc745043
aoihaugen <51489207+aoihaugen@users.noreply.github.com>
2023-01-21 22:17:57 +0100

    Comments and code cleaning.
    Comments has been added and function moved to end of code.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+//Constants for requiering API's
 const {Client, GatewayIntentBits} = require('discord.js')
 require('dotenv').config()
 const { Configuration, OpenAIApi } = require("openai");
-
-
+//Discord config
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -11,34 +11,33 @@ const client = new Client({
     ],
  
 })
-
+//Openai config
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-
-async function openAiRequest(question){
-    const completion = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: question,
-  max_tokens: 2000,
-});
-
-return completion.data.choices[0].text;
-}
-
+//Main program listening to messages on discord server.
 client.on('ready', () => {
     console.log('The bot is ready!')
 })
 
 client.on('messageCreate' , async message => {
     if (message.content.startsWith('AI:')) {
-        
+        //call to openAiRequest function with any message starting with "AI:", removes first 3 char. When function returns the reply is sent to discord bot, the bot replys to the person prompting.
         message.reply(await openAiRequest(message.content.substring(3)))
-
     }
 })
 
-
+//Logs in the bot
 client.login(process.env.TOKEN)
+
+//Function to send the question to OpenAI. Returns the answer from the openAI server
+async function openAiRequest(question){
+    const completion = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: question,
+  max_tokens: 2000,
+});
+return completion.data.choices[0].text;
+}
